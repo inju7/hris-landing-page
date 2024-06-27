ff053ee3cf91dba1bf678875d520dddaa906f6a2
Bea0928 <markmicobanzuela30@gmail.com>
2024-06-27 12:17:46 +0800

    Unfinished

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zjimnxnzjzcdsnmqmqeg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqaW1ueG56anpjZHNubXFtcWVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NTU4MTYsImV4cCI6MjAzNTAzMTgxNn0.fqRCjCd9q03Qorbyxxm1aA2eXw0ucKFvGT0Nt84WgWA'
+const supabaseUrl = 'http://127.0.0.1:54321'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function submitForm(e) {
